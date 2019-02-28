02822ecda1706224ab612999d30760fd245a7119
vaaceves <alexis@thinkof.tech>
2019-02-28 16:51:33 -0600

    assets

--- PlatziVideoApp/App.js
+++ PlatziVideoApp/App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,9 +22,18 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Image
+            source={require('./assets/logo.png')}
+            style={{ width:300, height:90}}
+        />
+        <Text
+            style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text
+            style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
       </View>
     );
   }
