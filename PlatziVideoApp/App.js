39271c4cdae4c1f22379a7189f4c43d2503e31e9
vaaceves <alexis@thinkof.tech>
2019-04-12 19:10:10 -0500

    applayout - components reorganizados

--- PlatziVideoApp/App.js
+++ PlatziVideoApp/App.js
@@ -1,47 +1,14 @@
 import React, {Component} from 'react';
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    Image,
-} from 'react-native';
-
-import Home from './src/screens/containers/home';
-import Header from './src/sections/components/header';
-import SuggestionList from './src/videos/containers/suggestion-list';
-import CategoryList from './src/videos/containers/category-list';
-import Player from './src/player/containers/player';
-import Loading from './src/videos/components/loading';
-
-import API from './utils/api';
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
+
 import { store, persistor } from "./store";
+import Loading from './src/videos/components/loading';
+import AppLayout from './src/app';
 
 
 type Props = {};
 export default class App extends Component<Props> {
-    state = {
-
-    }
-    async componentDidMount() {
-        const suggestionList = await API.getSuggestion(10);
-        store.dispatch({
-            type: 'SET_SUGGESTION_LIST',
-            payload: {
-                suggestionList
-            }
-        })
-        const categoryList = await API.getMovies();
-        store.dispatch({
-            type: 'SET_CATEGORY_LIST',
-            payload: {
-                categoryList
-            }
-        })
-    }
-
     render() {
     return (
         <Provider
@@ -51,12 +18,7 @@ export default class App extends Component<Props> {
                 loading = {<Loading />}
                 persistor = {persistor}
             >
-                <Home>
-                    <Header />
-                    <Player />
-                    <CategoryList />
-                    <SuggestionList />
-                </Home>
+                <AppLayout />
             </PersistGate>
         </Provider>
     );
